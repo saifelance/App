fa0322ad5bb9d9976c1661b82263f1d1c0fa765f
saifelance <saifelance@gmail.com>
2025-04-19 10:31:38 +0500

    Eslint fixeed

--- src/components/SettlementButton/index.tsx
+++ src/components/SettlementButton/index.tsx
@@ -89,7 +89,7 @@ function SettlementButton({
             if (typeof paymentMethod?.[policyIDKey] === 'string') {
                 return paymentMethod?.[policyIDKey];
             }
-            return (paymentMethod?.[policyIDKey]!)?.lastUsed;
+            return (paymentMethod?.[policyIDKey] as LastPaymentMethodType)?.lastUsed;
         },
         canBeMissing: true,
     });
